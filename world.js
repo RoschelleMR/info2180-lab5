cba23b4ca83f507a543e23335e9355c221098abf
RoschelleMR <roschelle2k6@gmail.com>
2022-11-12 03:38:32 -0500

    Implemented city lookup

--- world.js
+++ world.js
@@ -1,13 +1,34 @@
 window.addEventListener("load", function () {
-  let btn = document.getElementById("lookup");
+  let coun_btn = document.getElementById("country_lookup");
+  let city_btn = document.getElementById("city_lookup");
   let result = document.querySelector("#result");
   let input = document.querySelector("#country");
 
-  btn.addEventListener("click", function (event) {
+  coun_btn.addEventListener("click", function (event) {
     event.preventDefault();
 
     let input_value = input.value;
-    let url = `world.php?country=${input_value}`;
+    let url = `world.php?country=${input_value}&lookup=countries`;
+    console.log(url);
+
+    fetch(url)
+      .then(function (response) {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error(response.statusText);
+        }
+      })
+      .then(function (data) {
+        result.innerHTML = data;
+      });
+  });
+
+  city_btn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    let input_value = input.value;
+    let url = `world.php?country=${input_value}&lookup=cities`;
 
     fetch(url)
       .then(function (response) {
--- world.php
+++ world.php
@@ -9,17 +9,29 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 // $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
+$user_Input = $_GET['country']; //get user's entry of country
+$country = htmlspecialchars($user_Input); //sanitizing the input
+$lookup = $_GET["lookup"];
 
-$country = $_GET['country']; //get user's entry of country
-$user_Input = htmlspecialchars($country, ENT_COMPAT); //sanitizing the input
 
 
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if ($lookup === "countries"){
+  
+  echo country_table($conn, $country);
+}
 
-echo country_table($results);
+else if ($lookup === "cities"){
+  echo city_table($conn,$country);
+}
 
-function country_table($results){
+
+
+
+
+function country_table($conn, $country){
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
   echo "<table>";
   echo "<tr>";
   echo "<th>Name</th>";
@@ -36,8 +48,38 @@ function country_table($results){
     echo "<td>".$row["head_of_state"]."</td>";
     echo "</tr>";
   }
-  
+
   echo "</table>";
+}
+
+function city_table($conn, $country){
+  $stmt = $conn->prepare("SELECT cities.name, cities.district, cities.population FROM cities INNER JOIN countries ON countries.code = cities.country_code WHERE countries.name = :country");
+  $stmt->bindParam(':country',$country,PDO::PARAM_STR);
+  $stmt->execute();
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  if (count($results)>0){
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>Name</th>";
+    echo "<th>District</th>";
+    echo "<th>Population</th>";
+    echo "</tr>";
+    
+    foreach($results as $row){
+      echo "<tr>";
+      echo "<td>".$row['name']."</td>";
+      echo "<td>".$row["district"]."</td>";
+      echo "<td>".$row["population"]."</td>";
+      echo "</tr>";
+    }
+
+    echo "</table>";
+  }
+  else{
+    echo "$country is not a country";
+  }
+ 
 }
 
 ?>

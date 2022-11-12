ba9da85beb9cb03687f077edb95d9e883b07ee17
RoschelleMR <roschelle2k6@gmail.com>
2022-11-12 00:02:03 -0500

    Implemented Table Format Counrty Lookup in world.php

--- world.php
+++ world.php
@@ -17,18 +17,27 @@ $user_Input = htmlspecialchars($country, ENT_COMPAT); //sanitizing the input
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-foreach($results as $row){
-  echo "<ul>";
-  echo "<li>Name: ".$row['name'];
-  echo " ";
-  echo "Continent: ".$row["continent"];
-  echo " ";
-  echo "Region: ".$row["independence_year"];
-  echo " ";
-  echo "Head of State: ".$row["head_of_state"];
-  echo "</li>";
-  echo "<br>";
-  echo "</ul>";
+echo country_table($results);
+
+function country_table($results){
+  echo "<table>";
+  echo "<tr>";
+  echo "<th>Name</th>";
+  echo "<th>Continent</th>";
+  echo "<th>Independence</th>";
+  echo "<th>Head of State</th>";
+  echo "</tr>";
+
+  foreach($results as $row){
+    echo "<tr>";
+    echo "<td>".$row['name']."</td>";
+    echo "<td>".$row["continent"]."</td>";
+    echo "<td>".$row["independence_year"]."</td>";
+    echo "<td>".$row["head_of_state"]."</td>";
+    echo "</tr>";
+  }
+  
+  echo "</table>";
 }
 
 ?>
